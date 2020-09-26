08434d6f31650e0815d5cd9cad6a3700b926809b
Elyvic <elyvic.cabais@yahoo.com>
2020-09-25 19:14:41 -0500

    the program makes a downloads folder and put the files there

--- main.py
+++ main.py
@@ -1,26 +1,40 @@
 from tkinter import *
 import youtube_dl
+import os
 
 def Download():
+    #grab the string in the entry box
     global urlString
     urlString = urlEntry.get()
+
+    if not os.path.exists('./downloads'):
+        os.makedirs('./downloads')
+
+    #to get info of the url inputted on the entry
     video_info = youtube_dl.YoutubeDL().extract_info(url= urlString, download = False)
     filename = f"{video_info['title']}" + ".%(ext)s"
     options = {
         'format': 'bestaudio/best',
         'keepvideo': False,
-        'outtmpl': filename,
+        'outtmpl': '~/Desktop/Python Projects/YoutubeToMP3/downloads/' + filename,
         'postprocessors':[{
             'key': 'FFmpegExtractAudio',
             'preferredcodec': 'mp3',
             'preferredquality': '192',
         }]
     }
+
+    #this downloads the video and converts it to mp3
     with youtube_dl.YoutubeDL(options) as ydl:
         ydl.download([video_info['webpage_url']])
 
+    urlEntry.delete(0,END)
+    urlEntry.insert(0, "")
 
 
+
+
+#GUI stuff
 urlString = " "
 
 root = Tk()
